8aa7ec70d592b04648dea9095096817df5f68007
Regis Pires Magalhaes <regispiresmag@gmail.com>
2015-02-12 15:02:26 -0300

    first commit

---
+++ src/main/java/org/graphast/example/App.java
@@ -0,0 +1,16 @@
+package org.graphast.example;
+
+import org.graphast.config.Configuration;
+import org.graphast.importer.OSMImporterImpl;
+import org.graphast.model.Graph;
+
+public class App {
+	public static void main( String[] args ) {
+		String osmFile = App.class.getResource("/monaco-150112.osm.pbf").getPath();
+		String graphastMonacoDir = Configuration.USER_HOME + "/graphast/monaco";
+		
+		Graph graph = new OSMImporterImpl(osmFile, graphastMonacoDir).execute();
+		System.out.println("Number of nodes:" + graph.getNumberOfNodes());
+		System.out.println("Number of edges:" + graph.getNumberOfEdges());
+	}
+}
