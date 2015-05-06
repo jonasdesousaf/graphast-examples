1037647a4eaf914b48eb5a3c53b38c3cd779fefe
Regis Pires Magalhaes <regispiresmag@gmail.com>
2015-05-06 11:46:25 -0300

    Added a simple graph manual construction example with a shortest path algorithm execution

---
+++ src/main/java/org/graphast/example/GraphExample.java
@@ -0,0 +1,103 @@
+package org.graphast.example;
+
+import org.graphast.config.Configuration;
+import org.graphast.model.Edge;
+import org.graphast.model.EdgeImpl;
+import org.graphast.model.Graph;
+import org.graphast.model.GraphImpl;
+import org.graphast.model.NodeImpl;
+import org.graphast.query.route.shortestpath.ShortestPathService;
+import org.graphast.query.route.shortestpath.dijkstra.DijkstraConstantWeight;
+import org.graphast.query.route.shortestpath.model.Path;
+
+public class GraphExample {
+
+	public Graph generateExample() {
+
+		Graph graph = new GraphImpl(Configuration.USER_HOME + "/graphast/example");
+
+		Edge e;
+		NodeImpl v;
+
+		v = new NodeImpl(0l, 0d, 10d, 0);
+		graph.addNode(v);
+
+		v = new NodeImpl(1l, 10d, 0d, 1);
+		graph.addNode(v);
+
+		v = new NodeImpl(2l, 30d, 20d, 2);
+		graph.addNode(v);
+
+		v = new NodeImpl(3l, 40d, 20d, 4);
+		graph.addNode(v);
+
+		v = new NodeImpl(4l, 50d, 30d, 0);
+		graph.addNode(v);
+
+		v = new NodeImpl(5l, 60d, 20d);
+		graph.addNode(v);
+
+		v = new NodeImpl(6l, 60d, 0d);
+		graph.addNode(v);
+
+		int[] costs = {3,2,3,4};
+		e = new EdgeImpl(0l, 1l, 1, costs, null, "Rua A");
+		graph.addEdge(e);
+
+		costs = new int[]{4,2,6,8,10};
+		e = new EdgeImpl(0l, 2l, 5, costs, null, "Rua B");
+		graph.addEdge(e);
+
+		costs = new int[]{1,2};
+		e = new EdgeImpl(1l, 2l, 3, costs, null, "Rua C");
+		graph.addEdge(e);
+
+		costs = new int[]{4,4,7,6,11};
+
+		e = new EdgeImpl(2l, 3l, 3, costs, null, "Rua D");
+
+		graph.addEdge(e);
+
+		costs = new int[]{1,10};
+		e = new EdgeImpl(3l, 4l, 3, costs, null, "Rua E");
+		graph.addEdge(e);
+
+		costs = new int[]{2,12,13};
+		e = new EdgeImpl(3l, 5l, 4, costs, null, "Rua F");
+		graph.addEdge(e);
+
+		costs = new int[]{3,9,10,11};
+		e = new EdgeImpl(4l, 5l, 2, costs, null, "Rua G");
+		graph.addEdge(e);
+
+		costs = new int[]{5,2,4,6,8,15};
+		e = new EdgeImpl(5l, 6l, 1, costs, null, "Rua H");
+		graph.addEdge(e);
+
+		return graph;
+
+	}
+
+	
+	public static void main(String[] args) {
+		Graph graph = new GraphExample().generateExample();
+		ShortestPathService shortestPath = new DijkstraConstantWeight(graph);
+		Path path = shortestPath.shortestPath(0, 6);
+		System.out.println("The Shortest Path distance between 0 and 6 is " + path.getPathCost());
+		System.out.println("The Shortest Path distance between 0 and 6 is " + path.getPath());
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/org/graphast/example/ImporterApp.java
+++ src/main/java/org/graphast/example/ImporterApp.java
@@ -4,9 +4,9 @@ import org.graphast.config.Configuration;
 import org.graphast.importer.OSMImporterImpl;
 import org.graphast.model.Graph;
 
-public class App {
+public class ImporterApp {
 	public static void main( String[] args ) {
-		String osmFile = App.class.getResource("/monaco-150112.osm.pbf").getPath();
+		String osmFile = ImporterApp.class.getResource("/monaco-150112.osm.pbf").getPath();
 		String graphastMonacoDir = Configuration.USER_HOME + "/graphast/monaco";
 		
 		Graph graph = new OSMImporterImpl(osmFile, graphastMonacoDir).execute();
